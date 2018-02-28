ab3773edc6b8968406d7bfe49feebce8443807b7
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-02-28 16:39:19 +0530

    space.c
    good

---
+++ space.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+file*fp;
+char ch;
+int space=0;line=0;
+if(fp==null)
+{
+printf("\n  file not fount");
+exit;
+else
+}
+if(ch=EOF)
+{
+if (ch=' ');
+space++;
+if(ch=  \t);
+line++
+printf("\n number of space");
+printf(\n number of line");
+return 0
+}
